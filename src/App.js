1f936f598e1b3ee66ca07a754b25e0a3484beb9a
William Lindner <wlindner522@gmail.com>
2015-07-18 21:27:41 -0700

    Added React Router and implemented routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Router from 'react-router';  
+import { RouteHandler, Route } from 'react-router';
 import Home from './pages/Home';
 
-React.render(
-  <Home />,
-  document.getElementById('app')
+class App extends React.Component {
+  render() {
+    return (
+        <div id="app">
+            <div className="nav"></div>
+            <RouteHandler/>
+        </div>
+    );
+  }
+}
+
+var routes = (  
+  <Route handler={App}>
+    <Route path="/" handler={Home}/>
+  </Route>
 );
+
+Router.run(routes, Router.HashLocation, (Root) => {
+  React.render(<Root/>, document.body);
+});
