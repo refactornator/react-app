2917aa7aeaefd40801b53c8405ecb4fc2d7ae866
William Lindner <wlindner522@gmail.com>
2015-07-18 20:55:59 -0700

    Listen for resize events and update the columns in the home screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,23 +6,30 @@ import _ from 'lodash';
 import ChartGroup from '../components/ChartGroup';
 
 class Home extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
+        this.state = {windowWidth: window.innerWidth};
+
         this._handleClick = this._handleClick.bind(this);
+        this._handleResize = this._handleResize.bind(this);
     }
 
-    elementInfiniteLoad() {
-        return <div className="infinite-list-item">
-            Loading...
-        </div>;
+    componentDidMount() {
+        window.addEventListener('resize', this._handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this._handleResize);
     }
 
     render() {
+        var numberOfColumns = Math.round(this.state.windowWidth / 380);
+        
         return (
             <div style={styles.main}>
                 <Infinite containerHeight={document.body.clientHeight} elementHeight={500}>
                     {_.range(100).map(function(index) {
-                        return <ChartGroup rows={2} columns={3} key={index} />;
+                        return <ChartGroup rows={2} columns={numberOfColumns} key={index} />;
                     })}
                 </Infinite>
             </div>
@@ -31,6 +38,10 @@ class Home extends React.Component {
 
     _handleClick() {
         console.log(this);
+    }
+
+    _handleResize() {
+        this.setState({windowWidth: window.innerWidth});
     }
 }
 
